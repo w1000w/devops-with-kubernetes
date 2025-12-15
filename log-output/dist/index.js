8411a07b53268d0d4ef42291481b0a202c52a2b7
JKK <intergalaktinen@gmail.com>
2025-12-15 15:59:56 +0200

    Remove dist from repository

--- log-output/dist/index.js
+++
@@ -1,9 +0,0 @@
-import { v4 as uuidv4 } from "uuid";
-function generateRandomString() {
-    const randomString = uuidv4();
-    setInterval(() => {
-        console.log(randomString);
-    }, 5000);
-}
-generateRandomString();
-//# sourceMappingURL=index.js.map
