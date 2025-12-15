132ef84eaa30c35b4d17234f1f7841a929674aee
JKK <intergalaktinen@gmail.com>
2025-12-15 16:34:19 +0200

    add: Dockerfile

--- log-output/index.ts
+++ log-output/index.ts
@@ -2,6 +2,7 @@ import { v4 as uuidv4 } from "uuid";
 
 function generateRandomString() {
   const randomString = uuidv4();
+  console.log(randomString);
   setInterval(() => {
     console.log(randomString);
   }, 5000);
